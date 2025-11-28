ffa55ea9c31eea1e88cec5733dd820dfdefbffc6
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2025-11-28 13:41:02 +0800

    update github link

--- src/lib/constants/socialLinks.ts
+++ src/lib/constants/socialLinks.ts
@@ -9,7 +9,7 @@ export interface SocialLinksProps {
 
 export const socialLinks =
 {
-  github: "https://github.com/Jehuuuuu",
+  github: "https://github.com/jehu-rodriguez",
   twitter: "https://x.com/J3huuu",
   facebook: "https://www.facebook.com/JehuRodriguez017/",
   linkedin: "https://www.linkedin.com/in/jehu-rodriguez-a57051398",
